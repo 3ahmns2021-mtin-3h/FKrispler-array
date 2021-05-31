5b3d9fe8a7e23c3fb9a71f9b27b53d9aaf642b13
FKrispler <Florian.Krispler03@htl-salzburg.ac.at>
2021-05-31 13:14:01 +0200

    Aufgabe 3

--- Assets/MyGame/Scripts/PracticeArray.cs
+++ Assets/MyGame/Scripts/PracticeArray.cs
@@ -8,7 +8,7 @@ public class PracticeArray : MonoBehaviour
     public int[] array2 = new int[4] { 21, 22, 23, 24 };
     public int[] array3 = { 31, 32, 33, 34, 35, 36, 37 };
     public string[] array4 = new string[10];
-    public GameObject[] arrayColoredImagesRed;
+    public GameObject[] myRedImages;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,20 +17,25 @@ public class PracticeArray : MonoBehaviour
             array4[i] = "array4";
          }
 
-        arrayColoredImagesRed
-        = GameObject.FindGameObjectsWithTag("RedImage");
+        myRedImages = GameObject.FindGameObjectsWithTag("RedImage");
+
+        foreach(GameObject elem in myRedImages)
+        {
+            Debug.Log(elem.name);
+        }
+
 
         Debug.Log("Element 0 von array 1 hat den Wert: " + array1[0]);
         Debug.Log("Element 0 von array 2 hat den Wert: " + array2[0]);
         Debug.Log("Element 0 von array 3 hat den Wert: " + array3[0]);
         Debug.Log("Element 0 von array 4 hat den Wert: " + array4[0]);
-        Debug.Log("Name des ersten GameObjects: " + arrayColoredImagesRed);
+        Debug.Log("Name des ersten GameObjects: " + myRedImages);
 
         Debug.Log("Das Letzte Element von array 1 hat den Wert: " + array1[array1.Length - 1]);
         Debug.Log("Das Letzte Element von 2 hat den Wert: " + array2[array2.Length - 1]);
         Debug.Log("Das Letzte Element von 3 hat den Wert: " + array3[array3.Length - 1]);
         Debug.Log("Das Letzte Element von 4 hat den Wert: " + array4[array4.Length - 1]);
-        Debug.Log("Name des ersten GameObjects: " + arrayColoredImagesRed[arrayColoredImagesRed.Length - 1].name);
+        Debug.Log("Name des ersten GameObjects: " + myRedImages[myRedImages.Length - 1].name);
 
         PrintElements(array4);  
     }
